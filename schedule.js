f104dc5e19107c93711539b9f1a265ee9d58d70f
Paradox <parax_85@mail.ru>
2015-05-20 02:28:20 +0300

    test

--- schedule.js
+++ schedule.js
@@ -1,11 +1,15 @@
 var cron = require('node-schedule');
 var http = require('http');
 
+var server = http.createServer();
+server.listen(process.env.PORT);
+console.log('server started??');
+
 /* This runs at 3:10AM every Friday, Saturday and Sunday. */
 var rule2 = new cron.RecurrenceRule();
-rule2.dayOfWeek = [3];
-rule2.hour = 2;
-rule2.minute = 14;
+rule2.dayOfWeek = [2];
+rule2.hour = 23;
+rule2.minute = 30;
 cron.scheduleJob(rule2, function(){
     http.request({host:'doubledox.ru', path:'/Other/Some.aspx?test=test'}).end();
 	console.log('request sent');
