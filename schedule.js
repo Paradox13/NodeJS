74e6a161f59cdc9f844c79e2e762d446a12dfdf0
Paradox <parax_85@mail.ru>
2015-05-20 02:45:04 +0300

    mail

--- schedule.js
+++ schedule.js
@@ -1,5 +1,6 @@
 var cron = require('node-schedule');
 var http = require('http');
+var nodemailer = require('nodemailer');
 
 var server = http.createServer();
 server.listen(process.env.PORT);
@@ -7,10 +8,35 @@ console.log('server started??');
 
 /* This runs at 3:10AM every Friday, Saturday and Sunday. */
 var rule2 = new cron.RecurrenceRule();
-rule2.dayOfWeek = [2];
-rule2.hour = 23;
-rule2.minute = 36;
+rule2.dayOfWeek = [process.env.day];
+rule2.hour = process.env.hour;
+rule2.minute = process.env.minute;
+
+var transporter = nodemailer.createTransport({
+    service: 'Yandex',
+    auth: {
+        user: process.env.user,
+        pass: process.env.pass
+    }
+});
+
 cron.scheduleJob(rule2, function(){
     http.request({host:'doubledox.ru', path:'/Other/Some.aspx?test=test'}).end();
 	console.log('request sent');
+	
+	var mailOptions = {
+		from: 'DoubleeDox', // sender address
+		to: 'parax_85@mail.ru', // list of receivers
+		subject: 'Hello', // Subject line
+		text: 'Hello world', // plaintext body
+		html: '<b>Hello world</b>' // html body
+	};
+	
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+		}else{
+			console.log('Message sent: ' + info.response);
+		}
+	});
 });
