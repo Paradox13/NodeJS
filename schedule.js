7a3a16c107f8b5467d84901685c42ee4c9a1c476
Paradox <parax_85@mail.ru>
2015-05-20 03:04:12 +0300

    parseint

--- schedule.js
+++ schedule.js
@@ -1,6 +1,5 @@
 var cron = require('node-schedule');
 var http = require('http');
-var nodemailer = require('nodemailer');
 
 var server = http.createServer();
 server.listen(process.env.PORT);
@@ -8,9 +7,9 @@ console.log('server started');
 
 /* This runs at 3:10AM every Friday, Saturday and Sunday. */
 var rule2 = new cron.RecurrenceRule();
-rule2.dayOfWeek = [process.env.day];
-rule2.hour = process.env.hour;
-rule2.minute = process.env.minute;
+rule2.dayOfWeek = [parseInt(process.env.day)];
+rule2.hour = parseInt(process.env.hour);
+rule2.minute = parseInt(process.env.minute);
 
 console.log('on ' + rule2.dayOfWeek + ' at ' + rule2.hour + ':' + rule2.minute);
 
